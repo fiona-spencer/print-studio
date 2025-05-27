4d856259b5d4e8242499216ab6224a7162f011bf
Fiona Spencer <fionagspencer@gmail.com>
2025-05-27 02:52:02 -0400

    tailwind css working

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite-react/lib/esm/**/*.js",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        fadeInUp: 'fadeInUp 0.6s ease-out forwards',
+      },
+      keyframes: {
+        fadeInUp: {
+          '0%': { opacity: 0, transform: 'translateY(-20px)' },
+          '100%': { opacity: 1, transform: 'translateY(0)' },
+        },
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    require("flowbite/plugin"),
+  ],
 }
-
